65a30c532d3ea78c75027979f04e71ad272c863d
ViictorTorres21 <victor.torres.ortiz.18@gmail.com>
2023-01-11 12:36:43 +0100

    Cambios

--- PDO/register.php
+++ PDO/register.php
@@ -44,8 +44,7 @@
     if(empty($name)){
       $name_err = 'Please enter name';
     }
-
-    // Validar contraseña
+      // Validar contraseña
     if(empty($password)){
       $password_err = 'Please enter password';
     } elseif(strlen($password) < 6){
@@ -98,7 +97,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
   <title>Register An Account</title>
 </head>
-<body class="bg-primary">
+<body>
   <div class="container">
     <div class="row">
       <div class="col-md-6 mx-auto">
