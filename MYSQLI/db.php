c9bd5e2c38f73861c1e0d6059e0b5a037c4b0dd4
Víctor Torres Ortiz <114647477+VictorTorresOrtiz@users.noreply.github.com>
2023-01-12 19:11:13 +0000

    cambios en nombre var

--- MYSQLI/db.php
+++ MYSQLI/db.php
@@ -1,10 +1,10 @@
 <?php
    $servername = "localhost";
-   $database = "lol";
+   $db = "lol";
    $username = "root";
    $password = "";
    // Create connection
-   $conn = mysqli_connect($servername, $username, $password, $database);
+   $conn = mysqli_connect($servername, $username, $password, $db);
    // Check connection
    if (!$conn) {
        echo("Connection failed: " . mysqli_connect_error());
